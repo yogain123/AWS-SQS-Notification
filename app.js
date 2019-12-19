41c2c541216b14d9e95b16fdddfc5a1032c4b542
yogendra saxena <yogendrasaxena56@gmail.com>
2019-12-19 13:02:12 +0530

    added port
    Adding port so that when PORT provided in the environment it will use that port

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const port = 3060;
+const port = process.env.PORT || 3060;
 
 const app = express();
 
 app.use(bodyParser.json());
 
-
 app.use('/', require(__dirname + "/routes"));
-
-
 
 // listen for requests
 app.listen(port, () => {
